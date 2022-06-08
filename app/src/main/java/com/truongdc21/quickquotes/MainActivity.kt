ea8cfdd729dfc54ad92871670114d092bd107607
truongdc21 <76480051+truongdc21@users.noreply.github.com>
2022-06-08 11:53:47 +0700

    Update app/src/main/java/com/truongdc21/quickquotes/MainActivity.kt
    Co-authored-by: Le Quang Dao <90740771+daolq-2712@users.noreply.github.com>

--- app/src/main/java/com/truongdc21/quickquotes/MainActivity.kt
+++ app/src/main/java/com/truongdc21/quickquotes/MainActivity.kt
@@ -14,6 +14,7 @@ class MainActivity : AppCompatActivity() {
     
     private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
     var itemIDBottomNav : Int = 0
+    
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
